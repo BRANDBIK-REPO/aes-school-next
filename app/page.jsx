46f4d3de70dbbea612a5fb263613ac90625a8074
Najib <44539538+N4J13@users.noreply.github.com>
2023-05-16 13:49:54 +0530

    Added Vercel Analytics

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,10 @@
-
-
 import Hero from "@components/Hero";
 import CoursesSection from "@components/CoursesSection";
 import Features from "@components/Features";
 import AboutSection from "@components/AboutSection";
 import Faq from "@components/Faq";
 import TestimonialSection from "@components/TestimonialSection";
+import { Analytics } from '@vercel/analytics/react';
 
 const Home = () => {
   return (
@@ -16,6 +15,7 @@ const Home = () => {
       <AboutSection />
       <TestimonialSection />
       <Faq />  
+      <Analytics />
     </>
   );
 };
