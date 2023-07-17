903c151f601e35efecf80241079b5f8799d6dcfa
Najib <44539538+N4J13@users.noreply.github.com>
2023-07-17 14:21:14 +0530

    content added

--- constants/index.js
+++ constants/index.js
@@ -28,6 +28,35 @@ export const navLinks = [
   },
 ];
 
+export const footerLinks = [
+  {
+    title: "Quick Links",
+    links: [
+      { title: "Home", url: "/" },
+      { title: "About", url: "/about" },
+      { title: "Gallery", url: "/gallery" },
+      { title: "Contact", url: "/contact" },
+    ],
+  },
+  {
+    title: "Courses",
+    links: [
+      { title: "CA", url: "/courses/ca" },
+      { title: "CMA", url: "/courses/cma" },
+      { title: "CS", url: "/courses/cs" },
+      { title: "CATLAM", url: "/courses/catlam" },
+      { title: "IFRS", url: "/courses/ifrs" },
+    ],
+  },
+  {
+    title: "Privacy",
+    links: [
+      { title: "Terms and Conditions", url: "#" },
+      { title: "Privacy Policy", url: "#" },
+    ],
+  },
+];
+
 export const features = [
   {
     id: "1",
@@ -56,25 +85,24 @@ export const faqLeft = [
     id: 1,
     question: "How long does it take to complete CATLAM?",
     answer:
-      "Fringilla dictum tristique cras pellentesque morbi consequat, maecenas egestas a. Mi feugiat urna turpis placerat faucibus bibendum hac vulputate.",
+      "CATLAM is a 6-month in-house certificate program designed for individuals interested in a career in accounts, tax, and law. The course provides practical training in accounting, income tax laws, and accounting standards. It also covers practical knowledge in areas like goods and service tax and accounting software. CATLAM aims to equip students with the skills necessary for a sustainable career in the field.",
   },
   {
     id: 2,
     question: "What are the benefits of studying CATLAM ?",
-    answer:
-      "Fringilla dictum tristique cras pellentesque morbi consequat, maecenas egestas a. Mi feugiat urna turpis placerat faucibus bibendum hac vulputate.",
+    answer: "Practical skills in accounts, tax, and law Comprehensive knowledge in relevant subjects Independence and self-reliance in handling accounting matters Development of management skills Emphasis on practical application and adaptabilit Improved career prospects in accounting management.",
   },
   {
     id: 3,
     question: "Which course is better, CA or CMA ?",
     answer:
-      "Fringilla dictum tristique cras pellentesque morbi consequat, maecenas egestas a. Mi feugiat urna turpis placerat faucibus bibendum hac vulputate.",
+      "The choice between CA (Chartered Accountancy) and CMA (Cost and Management Accountancy) depends on your career goals and interests. CA primarily focuses on accounting, auditing, taxation, and financial reporting, while CMA emphasizes cost management, financial analysis, and strategic planning. Both courses have their unique advantages and career opportunities. It's essential to consider your preferences and long-term career aspirations to determine which course aligns better with your goals.",
   },
   {
     id: 4,
     question: "What is the difference between CA & CMA ?",
     answer:
-      "Fringilla dictum tristique cras pellentesque morbi consequat, maecenas egestas a. Mi feugiat urna turpis placerat faucibus bibendum hac vulputate.",
+      "CA (Chartered Accountancy) and CMA (Cost and Management Accountancy) differ in their focus and specialization. CA covers accounting, auditing, taxation, financial reporting, and business laws. CMA emphasizes cost management, financial analysis, strategic planning, and performance evaluation. Both designations have unique career opportunities. CA enables practice in auditing, tax consulting, and finance, while CMA is suitable for cost management, financial analysis, and strategic planning roles within organizations. Choose based on your interests and long-term goals.",
   },
 ];
 
@@ -83,89 +111,31 @@ export const faqRight = [
     id: 5,
     question: "Does this institute provide any scholorships ?",
     answer:
-      "Fringilla dictum tristique cras pellentesque morbi consequat, maecenas egestas a. Mi feugiat urna turpis placerat faucibus bibendum hac vulputate.",
+      "Yes, AES School of Commerce provides scholarships through entrance exams. The institute offers scholarships to deserving students based on their performance in the entrance examination. This provides an opportunity for talented individuals to receive financial assistance and support their education at the institute.",
   },
   {
     id: 6,
     question: "What is expected salary after  CA or CMA  ?",
     answer:
-      "Fringilla dictum tristique cras pellentesque morbi consequat, maecenas egestas a. Mi feugiat urna turpis placerat faucibus bibendum hac vulputate.",
+      "The salary after completing CA or CMA can vary based on factors like experience, job role, industry, and location. Both qualifications offer competitive salaries, with earning potential increasing as professionals gain experience and assume higher-level roles. It's advisable to research industry trends and consider various factors to get a better understanding of the expected salary range.",
   },
   {
     id: 7,
     question: "Can a student after 12th study, CA or CMA ?",
     answer:
-      "Fringilla dictum tristique cras pellentesque morbi consequat, maecenas egestas a. Mi feugiat urna turpis placerat faucibus bibendum hac vulputate.",
+      "Yes, students can pursue CA or CMA after completing their 12th grade. CA has a Foundation Course open to students from any stream, while CMA has a Foundation Course for students who have completed 10+2 from a recognized board. Both courses require dedication and rigorous study to obtain the qualifications.",
   },
   {
     id: 8,
     question: "Can i get job abroad if i study CATLAM or IFRS ?",
     answer:
-      "Fringilla dictum tristique cras pellentesque morbi consequat, maecenas egestas a. Mi feugiat urna turpis placerat faucibus bibendum hac vulputate.",
+      "Studying CATLAM or IFRS can enhance your job prospects abroad. Knowledge of international standards like IFRS can be beneficial for multinational companies, while CATLAM equips you with skills applicable globally. Additional qualifications, relevant experience, and research on job markets can improve your chances of securing international roles. It's important to consult with career counselors and immigration authorities for specific requirements.",
   },
 ];
 
-export const courses = [
-  {
-    id: 1,
-    title: "IFRS",
-    shortDescription:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac euet ac elit senectus mauris blandit tempore gestas.",
-    slug: "ifrs",
-    isImportant: true,
-  },
-  {
-    id: 2,
-    title: "CATLAM",
-    shortDescription:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac euet ac elit senectus mauris blandit tempore gestas.",
-    slug: "catlam",
-    isImportant: true,
-  },
-  {
-    id: 3,
-    title: "CA",
-    shortDescription:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac euet ac elit senectus mauris blandit tempore gestas.",
-    slug: "ca",
-    isImportant: false,
-  },
-  {
-    id: 4,
-    title: "CMA",
-    shortDescription:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac euet ac elit senectus mauris blandit tempore gestas.",
-    slug: "cma",
-    isImportant: false,
-  },
-  {
-    id: 5,
-    title: "CS",
-    shortDescription:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac euet ac elit senectus mauris blandit tempore gestas.",
-    slug: "cs",
-    isImportant: false,
-  },
-];
 
-export const ImportantCourses = [
-  {
-    id: 1,
-    title: "IFRS",
-    shortDescription:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac euet ac elit senectus mauris blandit tempore gestas.",
-    slug: "ifrs",
-    isImportant: true,
-  },
-  {
-    id: 2,
-    title: "CATLAM",
-    shortDescription:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac euet ac elit senectus mauris blandit tempore gestas.",
-    slug: "catlam",
-    isImportant: true,
-  },
-];
+
+
 
 export const testimonials = [
   {
@@ -206,41 +176,41 @@ export const offering = [
     icon: <Target color="white" strokeWidth={1} size={48} />,
     title: "Thrive for Results",
     desciption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac euet ac elit senectus mauris blandit tempore gestas.",
+      "AE's believe in achieving successful outcomes through determination and hard work.",
   },
   {
     id: 2,
     icon: <Zap color="white" strokeWidth={1} size={48} />,
     title: "Move Fast",
     desciption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac euet ac elit senectus mauris blandit tempore gestas.",
+      "AE's value agility and quick decision-making to seize opportunities and drive progress.",
   },
   {
     id: 3,
     icon: <Globe color="white" strokeWidth={1} size={48} />,
     title: "Connect with the world",
     desciption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac euet ac elit senectus mauris blandit tempore gestas.",
+      "AE's emphasize the importance of global connections and networking for personal and professional growth.",
   },
   {
     id: 4,
     icon: <Users2 color="white" strokeWidth={1} size={48} />,
     title: "Community building",
     desciption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac euet ac elit senectus mauris blandit tempore gestas.",
+      "AE's prioritize building strong communities to foster collaboration, support, and knowledge sharing.",
   },
   {
     id: 5,
     icon: <Briefcase color="white" strokeWidth={1} size={48} />,
     title: "Job placement assistance",
     desciption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac euet ac elit senectus mauris blandit tempore gestas.",
+      "AE's provide support in securing rewarding job opportunities for their students.",
   },
   {
     id: 6,
     icon: <BookOpen color="white" strokeWidth={1} size={48} />,
     title: "Industry driven curriculum",
     desciption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac euet ac elit senectus mauris blandit tempore gestas.",
+      " AE's offer relevant and up-to-date education aligned with industry needs",
   },
 ];
