51d181df88eb53d54d7cd145f9ee4813b37aee43
BRANDBIK CREATIVES <brandbikrepo@gmail.com>
2024-12-11 10:21:31 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -13,28 +13,6 @@ async function getData() {
   return products;
 }
 
-type="application/ld+json">
-{
-  "@context": "https://schema.org",
-  "@type": "LocalBusiness",
-  "name": "AE's School of Commerce",
-  "image": "https://www.aessoc.com/_next/image?url=%2Fassets%2Flogo.png&w=1920&q=75",
-  "@id": "",
-  "url": "https://www.aessoc.com/",
-  "telephone": "+91 80891 45551",
-  "address": {
-    "@type": "PostalAddress",
-    "streetAddress": "2nd floor, Sahari building, Jubilee Rd",
-    "addressLocality": "Perintalmanna, Malappuram",
-    "postalCode": "679322",
-    "addressCountry": "IN"
-  },
-  "geo": {
-    "@type": "GeoCoordinates",
-    "latitude": 10.9707316,
-    "longitude": 76.220496
-  }  
-}
 export const metadata = {
   title: "Top CMA Institute in Perinthalmanna | AE School of Commerce",
   description: `Best CMA Institute in Perinthalmanna, AE's School of Commerce, can help you realize your CMA goals by providing top-notch coaching and best practical training`,
